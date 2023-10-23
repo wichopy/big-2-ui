82b4ee42037ce104aa2a1e8d845079916e809423
Will Chou <will.chou@qlik.com>
2023-10-23 09:39:08 -0400

    fix rollup to do proxying (stupid cors)

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import dev from 'rollup-plugin-dev'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -35,9 +36,22 @@ export default {
     // https://github.com/rollup/rollup-plugin-commonjs
     resolve(),
     commonjs(),
-
+    !production && dev({
+      dirs: ['public'],
+      spa: 'public/index.html',
+      port: 3005,
+      proxy: [
+          { from: '/room', to: 'http://127.0.0.1:3000/room' },
+          { from: '/game', to: 'http://127.0.0.1:3000/game' },
+      ],
+      // host: '0.0.0.0',
+      force: true,
+  }),
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser()
-  ]
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
